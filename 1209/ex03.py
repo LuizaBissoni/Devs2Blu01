fb340b6681141d729b7e2d51f0ccbc1de23be410
Luiza Bissoni <113144012+LuizaBissoni@users.noreply.github.com>
2022-09-13 18:02:56 -0300

    Update ex03.py

--- 1209/ex03.py
+++ 1209/ex03.py
@@ -22,4 +22,4 @@ n1 = float(input('digite a primeira nota: '))
 n2 = float(input('digite a segunda nota: '))
 media =  (n1 + n2) / 2
 
-print('-'*15, '\n a primeira nota é: {}\n a segunda nota é: {} sua média é {}\n'.format(n1,n2,media),'-'*15
+print('-'*15, '\n a primeira nota é: {}\n a segunda nota é: {} sua média é {}\n'.format(n1,n2,media),'-'*15)

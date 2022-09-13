e14ff0da18a7bb6af82a3f1a990d7c481d591b34
Luiza Bissoni <113144012+LuizaBissoni@users.noreply.github.com>
2022-09-13 20:57:02 -0300

    atualização

---
+++ 13-09/ex05.py
@@ -0,0 +1,11 @@
+from time import sleep
+
+poli = "*"*20
+
+print(f"\n {poli} CABECALHO {poli} \n")
+
+for c in range(0 , 20):
+    print("     FELIZ DIA DO PROGRAMADOR     ")
+    sleep(2)
+    
+print(f"\n {poli} RODAPE {poli} \n")
---
+++ 13-09/ex06.py
@@ -0,0 +1,7 @@
+border = "="*5
+print(f"{border} CABEÇALHO {border}\n")
+for i in range(0, 1501, 2):
+    print(i)
+
+print("Parabens !!! Você conseguiu.\n")
+print(f"{border} RODAPÉ {border}\n")

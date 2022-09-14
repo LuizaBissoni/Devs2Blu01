5bdb93c78e90e0788c0a91d7a6a17fa3e850e911
Luiza Bissoni <113144012+LuizaBissoni@users.noreply.github.com>
2022-09-13 21:36:27 -0300

    atualização

--- 13-09/ex03.py
+++
@@ -1,9 +0,0 @@
-n1 = int(input('escreva um número inteiro: '))
-poli = '='*5
-
-print(f'\n {poli} INICIO {poli}\n')
-
-for c in range(0,n1+1):
-    print(c)
-
-print(f'\n {poli} FIM {poli}\n')
--- 13-09/ex06.py
+++
@@ -1,7 +0,0 @@
-border = "="*5
-print(f"{border} CABEÇALHO {border}\n")
-for i in range(0, 1501, 2):
-    print(i)
-
-print("Parabens !!! Você conseguiu.\n")
-print(f"{border} RODAPÉ {border}\n")

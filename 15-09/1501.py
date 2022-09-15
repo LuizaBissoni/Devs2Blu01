5aae769c7577f5590cddaae069825c852f3add5f
Luiza Bissoni <113144012+LuizaBissoni@users.noreply.github.com>
2022-09-15 19:40:51 -0300

    atualização

---
+++ 15-09/1501.py
@@ -0,0 +1,7 @@
+#dicionario
+
+dicionario = {'nome': 'Abacate', 'sobrenome': 'da silva'}
+
+#print(dicionario)
+
+print(dicionario['sobrenome'])
---
+++ 15-09/1502.py
@@ -0,0 +1,15 @@
+#dicionario
+from contextlib import AbstractAsyncContextManager
+
+
+dic = {}
+
+nome = str(input('diga seu nome: '))
+sobrenome = str(input('diga seu sobrenome: '))
+idade = int(input('quntos anos tu tem? '))
+
+dic['nome'] = nome
+dic['sobrenome'] = sobrenome
+dic['idade'] = idade
+
+print(dic)

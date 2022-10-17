c7649918020345641295cc7015f04f8cdb3dfa7d
Luiza Bissoni <luiza.bissoni@hotmail.com>
2022-10-17 20:59:22 -0300

    atualizando

--- calculadora/main.py
+++ calculadora/main.py
@@ -3,3 +3,32 @@
 # deve ser utilizado o conceito de interpolação para criar um cabeçalho de um menu, dentro deste menu deve conter a 
 # possibilidade de fazer a impressão dos dados inseridos pelo usuário, permitindo assim o usuário escolher uma das 4 operações 
 # matemáticas importadas, consequentemente imprimindo assim o resultado desejado dos dados inseridos.
+from controller import soma
+from controller import subtracao
+from controller import multiplicacao
+from controller import divisao
+
+poli = "=" * 10
+
+opcao=1
+
+while opcao:
+    print (f'\n {poli} Qual operação jovem? :) {poli} ')
+    print("1. Somar")
+    print("2. Subtrair")
+    print("3. Multiplicação")
+    print("4. Divisão ")
+
+    opcao = int(input("Opção: "))
+
+    if(opcao==1):
+        soma()
+    if(opcao==2):
+        subtracao()
+    if(opcao==3):
+        multiplicacao()
+    if(opcao==4):
+        divisao()
+
+
+

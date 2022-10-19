57a959fc9d793cf8f72f1559b3f1a84ee076946c
Luiza Bissoni <luiza.bissoni@hotmail.com>
2022-10-19 20:07:28 -0300

    atualizando

---
+++ dojo/main.py
@@ -0,0 +1,24 @@
+def main():
+    poli = '-' * 20
+    print(f'{poli} bem vindo {poli}')
+    print('funções'.center(52, ' '))
+    print('1 somar')
+    print('2 subtrair')
+    print('3 dividir')
+    print('4 multiplicar')
+    print('5 sair')
+
+    opcao = input('qual opção? ')
+
+    match opcao:
+        case '1':
+            pass 
+        case '2':
+            pass 
+        case '3':
+            pass
+        case '4':
+            pass 
+        case '_':
+            pass 
+main()
---
+++ dojo/maincalculadora.py
@@ -0,0 +1,21 @@
+from calculadora import soma, multi, divi, sub
+
+def menu():
+    print('*'*25, 'menu', '*'*25)
+
+    cond = 'sim'
+    while cond == 'sim':
+        n1 = int(input('numero'))
+        n2 = int(input('numero'))
+
+        Soma = soma(n1, n2)
+        Subtracao = sub(n1, n2)
+        Divisao = divi(n1, n2)
+        Multiplicacao = multi(n1, n2)
+    
+        print('a soma entre os numeros é: {}\n subtracao entre os numeros é: {}\na divisao entre os numeros é: {}\na multiplicacao entre os numeros é: {}\n'.format(Soma, Subtracao, Multiplicacao, Divisao))
+
+        cond = str(input('deseja continuar?\nsim\nnão')) 
+
+menu()
+print('saiu')

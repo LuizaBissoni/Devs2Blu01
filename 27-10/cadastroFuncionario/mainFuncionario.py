6855ab41662ca5c508ded9a23924ab1be3f87b31
Luiza Bissoni <luiza.bissoni@hotmail.com>
2022-10-27 22:07:01 -0300

    Create mainFuncionario.py

---
+++ 27-10/cadastroFuncionario/mainFuncionario.py
@@ -0,0 +1,7 @@
+# lista: conceito de indice, numeros
+# dicionario: chave e valor (alterável)
+# .strip() > tira espaços 
+# .capitalize() > deixa a primeira letra maiúscula 
+# .upper() transforma letra maiúscula
+# .items() pega somente o valor do dicionário
+# ''' três aspas para dar quebra de linha 

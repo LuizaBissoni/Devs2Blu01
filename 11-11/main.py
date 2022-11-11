037ff106dce6eace87370122b788abf4c665a581
Luiza Bissoni <luiza.bissoni@hotmail.com>
2022-11-11 20:00:26 -0300

    atualizando

--- 11-11/main.py
+++ 11-11/main.py
@@ -2,13 +2,13 @@ from conta import Conta
 
 print('~'*5, 'Conta', '~'*5)
 
-                #atributo titular
+               
 conta = Conta(input('Nome: '), 
-#atributo Numero Conta
+
 int(input('Conta: ')),
-#atributo Saldo Conta
+
 float(input('Saldo: ')),
-#atributo limite Conta
+
 float(input('Limite: ')))
 
 
